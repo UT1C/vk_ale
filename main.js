edd302a5543e2327165f80e2cf01765148553f7e
lightmanLP <liteman1000@gmail.com>
2021-11-20 01:49:23 +0700

    header

---
+++ main.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         vk_ale
+// @description  lets disable it.
+// @version      0.0.0.1
+// @author       lightmanLP & mikk357
+
+// @include      *://vk.com/*
+
+// @downloadURL  https://raw.githubusercontent.com/UT1C/vk_ale/release/main.js
+// @updateURL    https://raw.githubusercontent.com/UT1C/vk_ale/release/main.js
+// @supportURL   https://github.com/UT1C/vk_ale/issues
+
+// @license MIT
+// @homepageURL https://github.com/UT1C/vk_ale
+// ==/UserScript==
