3e8fdf639353780e7ede194db14846631c4d493a
lightmanLP <liteman1000@gmail.com>
2021-11-20 02:52:26 +0700

    init

--- main.js
+++ main.js
@@ -5,11 +5,36 @@
 // @author       lightmanLP & mikk357
 
 // @include      *://vk.com/*
-
-// @downloadURL  https://raw.githubusercontent.com/UT1C/vk_ale/release/main.js
-// @updateURL    https://raw.githubusercontent.com/UT1C/vk_ale/release/main.js
-// @supportURL   https://github.com/UT1C/vk_ale/issues
+// @run-at       document-start
 
 // @license MIT
 // @homepageURL https://github.com/UT1C/vk_ale
+// @supportURL   https://github.com/UT1C/vk_ale/issues
+// @downloadURL  https://raw.githubusercontent.com/UT1C/vk_ale/release/main.js
+// @updateURL    https://raw.githubusercontent.com/UT1C/vk_ale/release/main.js
 // ==/UserScript==
+
+// feed redirect disable
+if (location.href == "https://vk.com/feed" && origin == "https://vk.com")
+{
+    location.replace("https://vk.com/im");
+}
+
+// remove ecosystem menu
+document.addEventListener(
+    "DOMContentLoaded",
+    function (event) {
+        document.getElementById("top_ecosystem_navigation_link").remove();
+        document.getElementById("top_ecosystem_navigation_menu").remove();
+    }
+);
+
+// remove mini chat
+document.addEventListener(
+    "DOMContentLoaded",
+    function (event) {
+        document
+            .getElementsByClassName("FCPanel FCPanel--wide FCPanel")[0]
+            .remove();
+    }
+);
