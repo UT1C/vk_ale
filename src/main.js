418b4d17f1c325a20a398c1863db638c60277c72
lightmanLP <liteman1000@gmail.com>
2021-11-20 05:51:18 +0700

    0.0.0.1
    commit be56e967b45863f0e271d423981ef93a207d48cb
    Author: lightmanLP <liteman1000@gmail.com>
    Date:   Sat Nov 20 05:50:42 2021 +0700
    
        readme
    
    commit 3709235242dabdf7a2527f439c767ccfbbab36ee
    Author: lightmanLP <liteman1000@gmail.com>
    Date:   Sat Nov 20 05:45:18 2021 +0700
    
        restructure
    
    commit faa7b716499f56503bafbb3f3d7dfc712f629986
    Author: lightmanLP <liteman1000@gmail.com>
    Date:   Sat Nov 20 05:42:54 2021 +0700
    
        small rework

---
+++ src/main.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         vk_ale
+// @description  lets disable it.
+// @version      0.0.0.1
+// @author       lightmanLP & mikk357
+
+// @license      MIT
+// @homepageURL  https://github.com/UT1C/vk_ale
+// @supportURL   https://github.com/UT1C/vk_ale/issues
+// @downloadURL  https://raw.githubusercontent.com/UT1C/vk_ale/release/src/main.js
+// @updateURL    https://raw.githubusercontent.com/UT1C/vk_ale/release/src/main.js
+
+// @match      *://vk.com/*
+// @run-at       document-start
+
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://raw.githubusercontent.com/UT1C/vk_ale/release/src/utils.js
+// ==/UserScript==
+
+// feed redirect disable
+if (location.href == "https://vk.com/feed" && origin == "https://vk.com")
+{
+    location.replace("https://vk.com/im");
+}
+
+// remove ecosystem menu
+document.addEventListener(
+    "DOMContentLoaded",
+    function (event) {
+        deleteElement("#top_ecosystem_navigation_link");
+        deleteElement("#top_ecosystem_navigation_menu");
+    }
+);
+
+// remove mini chat
+document.addEventListener(
+    "DOMContentLoaded",
+    function (event) {
+        deleteElement("#fastchat-reforged");
+    }
+);
